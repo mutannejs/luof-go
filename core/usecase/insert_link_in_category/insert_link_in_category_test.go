0b5984f7af84d70c1a3c40feace03d130b85f599
Mutanne JS <murillojustinoklein@gmail.com>
2026-06-13 11:33:39 -0300

    test(insert_link_in_category): validação da existência da categoria e do link

--- core/usecase/insert_link_in_category/insert_link_in_category_test.go
+++ core/usecase/insert_link_in_category/insert_link_in_category_test.go
@@ -12,11 +12,13 @@ import (
 
 var (
 	alreadyBelongs = domain.ALREADY_BELONGS
+	categoryNotExists = domain.CATEGORY_NOT_EXISTS
+	linkNotExists = domain.LINK_NOT_EXISTS
 	mockUidLink = domain.MockUidLink
 	mockUidCategory = domain.MockUidCategory
 )
 
-func TestInsertLinkInCategory_NotExists(t *testing.T) {
+func TestInsertLinkInCategory_CategoryNotExists(t *testing.T) {
 	var assert = assert.New(t)
 
 	var btRepo = repository.NewBelongsToMockRepository()
@@ -42,11 +44,24 @@ func TestInsertLinkInCategory_NotExists(t *testing.T) {
 			mock.AnythingOfType("uuid.UUID"),
 		).Return(nil)
 
+	cRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil)
+
+	lRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
 	err := ilic.Execute(mockUidLink, mockUidCategory, true)
 
-	assert.NoError(
+	assert.Error(
+		categoryNotExists,
 		err,
-		"Tentar inserir um link em uma categoria, ambos ainda não relacionados, não deveria retornar erro")
+		"Tentar inserir um link em uma categoria que não existe deveria retornar erro contendo " + categoryNotExists.Error())
 }
 
 func TestInsertLinkInCategory_Exists(t *testing.T) {
@@ -75,10 +90,113 @@ func TestInsertLinkInCategory_Exists(t *testing.T) {
 			mock.AnythingOfType("uuid.UUID"),
 		).Return(nil)
 
+	cRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
+	lRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
 	err := ilic.Execute(mockUidLink, mockUidCategory, false)
 
 	assert.ErrorIs(
 		alreadyBelongs,
 		err,
 		"Tentar inserir um link em uma categoria, ambos já relacionados, deveria retornar erro contendo " + alreadyBelongs.Error())
+}
+
+func TestInsertLinkInCategory_LinkNotExists(t *testing.T) {
+	var assert = assert.New(t)
+
+	var btRepo = repository.NewBelongsToMockRepository()
+	var cRepo = repository.NewCategoryMockRepository()
+	var lRepo = repository.NewLinkMockRepository()
+	var ilic = New(btRepo, cRepo, lRepo)
+
+	btRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"Create",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("time.Time"),
+			true,
+		).Return(nil).
+		On(
+			"SetHasNoMainCategory",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(nil)
+
+	cRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
+	lRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil)
+
+	err := ilic.Execute(mockUidLink, mockUidCategory, true)
+
+	assert.Error(
+		linkNotExists,
+		err,
+		"Tentar inserir um link que não existe em uma categoria válida deveria retornar erro contendo " + linkNotExists.Error())
+}
+
+func TestInsertLinkInCategory_NotExists(t *testing.T) {
+	var assert = assert.New(t)
+
+	var btRepo = repository.NewBelongsToMockRepository()
+	var cRepo = repository.NewCategoryMockRepository()
+	var lRepo = repository.NewLinkMockRepository()
+	var ilic = New(btRepo, cRepo, lRepo)
+
+	btRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"Create",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("time.Time"),
+			true,
+		).Return(nil).
+		On(
+			"SetHasNoMainCategory",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(nil)
+
+	cRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
+	lRepo.
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil)
+
+	err := ilic.Execute(mockUidLink, mockUidCategory, true)
+
+	assert.NoError(
+		err,
+		"Tentar inserir um link em uma categoria, ambos ainda não relacionados, não deveria retornar erro")
 }
