dca7539ff46543d197aa55dc02a6eb1984d16f0a
Mutanne JS <murillojustinoklein@gmail.com>
2026-06-13 11:27:47 -0300

    test(insert_subcategory): validação da existência da categoria pai e filha

--- core/usecase/insert_subcategory/insert_subcategory_test.go
+++ core/usecase/insert_subcategory/insert_subcategory_test.go
@@ -12,13 +12,51 @@ import (
 
 var (
 	alternativeMockUidCategory = domain.AlternativeMockUidCategory
+	childNotExists = domain.CHILD_NOT_EXISTS
+	fatherNotExists = domain.FATHER_NOT_EXISTS
 	isAncestor = domain.ANCESTOR_NOT_BECOME_A_SUBCATEGORY
 	isSameCategory = domain.CANNOT_BE_A_SUBCATEGORY_OF_ITSELF
 	isSubcategory = domain.IS_SUBCATEGORY
 	mockUidCategory = domain.MockUidCategory
 )
 
-func TestInsertSubcategory_NotExists(t *testing.T) {
+func TestInsertSubcategory_AncestorBecomeASubcategory(t *testing.T) {
+	var assert = assert.New(t)
+
+	var cRepo = repository.NewCategoryMockRepository()
+	var is = New(cRepo)
+
+	cRepo.
+		On(
+			"IsSubcategory",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"IsAncestor",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil).
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil).
+		On(
+			"InsertSubcategory",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("time.Time"),
+		).Return(nil)
+
+	err := is.Execute(alternativeMockUidCategory, mockUidCategory)
+
+	assert.ErrorIs(
+		isAncestor,
+		err,
+		"Tentar inserir uma subcategoria em outra categoria, a primeira sendo ancestral da outra, deveria retornar erro contendo " + isAncestor.Error())
+}
+
+func TestInsertSubcategory_ChildNotExists(t *testing.T) {
 	var assert = assert.New(t)
 
 	var cRepo = repository.NewCategoryMockRepository()
@@ -36,6 +74,14 @@ func TestInsertSubcategory_NotExists(t *testing.T) {
 			mock.AnythingOfType("uuid.UUID"),
 		).Return(false, nil).
 		On(
+			"Exists",
+			alternativeMockUidCategory,
+		).Return(true, nil).
+		On(
+			"Exists",
+			mockUidCategory,
+		).Return(false, nil).
+		On(
 			"InsertSubcategory",
 			mock.AnythingOfType("uuid.UUID"),
 			mock.AnythingOfType("uuid.UUID"),
@@ -44,9 +90,10 @@ func TestInsertSubcategory_NotExists(t *testing.T) {
 
 	err := is.Execute(alternativeMockUidCategory, mockUidCategory)
 
-	assert.NoError(
+	assert.ErrorIs(
+		childNotExists,
 		err,
-		"Tentar inserir uma subcategoria em outra categoria, ambas não relacionadas, não deveria retornar erro")
+		"Tentar inserir uma subcategoria que não existe em uma categoria válida deveria retornar erro contendo " + childNotExists.Error())
 }
 
 func TestInsertSubcategory_Exists(t *testing.T) {
@@ -67,6 +114,10 @@ func TestInsertSubcategory_Exists(t *testing.T) {
 			mock.AnythingOfType("uuid.UUID"),
 		).Return(true, nil).
 		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil).
+		On(
 			"InsertSubcategory",
 			mock.AnythingOfType("uuid.UUID"),
 			mock.AnythingOfType("uuid.UUID"),
@@ -81,7 +132,7 @@ func TestInsertSubcategory_Exists(t *testing.T) {
 		"Tentar inserir uma subcategoria em outra categoria, ambas já relacionadas, deveria retornar erro contendo " + isSubcategory.Error())
 }
 
-func TestInsertSubcategory_AncestorBecomeASubcategory(t *testing.T) {
+func TestInsertSubcategory_FatherNotExists(t *testing.T) {
 	var assert = assert.New(t)
 
 	var cRepo = repository.NewCategoryMockRepository()
@@ -97,6 +148,14 @@ func TestInsertSubcategory_AncestorBecomeASubcategory(t *testing.T) {
 			"IsAncestor",
 			mock.AnythingOfType("uuid.UUID"),
 			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"Exists",
+			alternativeMockUidCategory,
+		).Return(false, nil).
+		On(
+			"Exists",
+			mockUidCategory,
 		).Return(true, nil).
 		On(
 			"InsertSubcategory",
@@ -108,9 +167,44 @@ func TestInsertSubcategory_AncestorBecomeASubcategory(t *testing.T) {
 	err := is.Execute(alternativeMockUidCategory, mockUidCategory)
 
 	assert.ErrorIs(
-		isAncestor,
+		fatherNotExists,
 		err,
-		"Tentar inserir uma subcategoria em outra categoria, a primeira sendo ancestral da outra, deveria retornar erro contendo " + isAncestor.Error())
+		"Tentar inserir uma subcategoria válida em uma categoria que não existe deveria retornar erro contendo " + fatherNotExists.Error())
+}
+
+func TestInsertSubcategory_NotExists(t *testing.T) {
+	var assert = assert.New(t)
+
+	var cRepo = repository.NewCategoryMockRepository()
+	var is = New(cRepo)
+
+	cRepo.
+		On(
+			"IsSubcategory",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"IsAncestor",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(false, nil).
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil).
+		On(
+			"InsertSubcategory",
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("time.Time"),
+		).Return(nil)
+
+	err := is.Execute(alternativeMockUidCategory, mockUidCategory)
+
+	assert.NoError(
+		err,
+		"Tentar inserir uma subcategoria em outra categoria, ambas não relacionadas, não deveria retornar erro")
 }
 
 func TestInsertSubcategory_SubcategoryOfItself(t *testing.T) {
@@ -130,6 +224,10 @@ func TestInsertSubcategory_SubcategoryOfItself(t *testing.T) {
 			mock.AnythingOfType("uuid.UUID"),
 			mock.AnythingOfType("uuid.UUID"),
 		).Return(false, nil).
+		On(
+			"Exists",
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(true, nil).
 		On(
 			"InsertSubcategory",
 			mock.AnythingOfType("uuid.UUID"),
